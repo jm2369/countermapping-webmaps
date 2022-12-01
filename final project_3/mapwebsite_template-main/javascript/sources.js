65748510d4c7855e32ef08873f18ffbe9d6caf62
jm2369 <jm2369@cornell.edu>
2022-12-01 00:55:47 -0500

    12/01
    update

---
+++ final project_3/mapwebsite_template-main/javascript/sources.js
@@ -0,0 +1,303 @@
+// zoomed in line
+// zoomed out fill with 1 opacity
+// add in hover box
+// slider for hover
+
+
+
+
+// Adding Sources
+function load_layers() {
+
+    map.addLayer({
+          id:'road',
+          type:'line',
+          source:{
+              'type':'vector',
+              'url':'mapbox://jm2369.0nxloer5'
+          },
+        'source-layer': 'Road_network-92m4hn',
+            'paint': {
+        'line-color': [
+                 "match",
+                  ["get", "status"],
+                  "Paved",
+                  "#f0ead6",
+                  "Unpaved",
+                  "#b7c8c2",/* other */ '#ccc'
+                ],
+        'line-width': [
+                 "match",
+                  ["get", "status"],
+                  "Paved",
+                  3,
+                  "Unpaved",
+                  1,/* other */ 1
+                ],
+
+        'line-opacity': 0.7
+      },
+          'layout': {
+          'visibility': 'none',
+           }
+      });
+    // Results
+    // blkgrp_exp
+/*    map.addLayer({
+        id:'education',
+        type:'symbol',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.075ktki3'
+        },
+        'source-layer': 'Education-98krso',
+
+});
+
+    map.addLayer({
+        id:'health',
+        type:'symbol',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.cma49xue'
+        },
+        'source-layer': 'health-2uy1h1',
+    });
+
+
+    map.addLayer({
+        id:'trade',
+        type:'symbol',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.3qju3t2l'
+        },
+        'source-layer': 'Tradecentre-4rn07x',
+    });
+*/
+
+
+
+
+    map.addLayer({
+        id:'water',
+        type:'fill',
+        source:{
+            'type':'vector',
+            'url':'jm2369.9sqwz8a5'
+        },
+      'source-layer': 'Waterbodies-7rb0f5',
+          'paint': {
+      'fill-color': [
+               "step",
+                ["get", "objectid"],
+                "#F7D6D0",
+                3,
+                "#F7D6D0",
+                5,
+                "#F7D6D0"
+              ],
+      'fill-opacity': 0.5
+    },
+        // 'layout': {
+        //     'visibility': 'none',
+        // }
+    });
+    map.addLayer({
+        id:'landslide',
+        type:'fill',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.7exvs94a'
+        },
+      'source-layer': 'Landslide_Risk_Areas-cj9lr6',
+                'paint': {
+      'fill-color': [
+               "step",
+                ["get", "gridcode"],
+                "#922c40",
+                3,
+                "#922c40",
+                5,
+                "#922c40"
+              ],
+      'fill-opacity': 0.8
+    },
+        'layout': {
+            'visibility': 'none',
+        }
+    });
+
+        map.addLayer({
+        id:'access1',
+        type:'fill',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.8gj7t9e5'
+        },
+        'source-layer': 'Accessibility_1-45hiww',
+        'paint': {
+            'fill-color':
+            [
+               "step",
+                ["get", "k_complexity"],
+                "#fcfdbf",
+                3,
+                "#fe9f6d",
+                5,
+                "#de4968",
+                10,
+                "#8c2981",
+                15,
+                "#3b0f70",
+                20,
+                "#000004"
+              ],
+              'fill-opacity': 0.4
+        },
+        'layout': {
+            'visibility': 'none',
+        }
+    });
+        map.addLayer({
+        id:'access2',
+        type:'fill',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.95h1htzm'
+        },
+        'source-layer': 'Accessibility_2-cd82gq',
+        'paint': {
+            'fill-color':
+            [
+               "step",
+                ["get", "k_complexity"],
+                "#fcfdbf",
+                3,
+                "#fe9f6d",
+                5,
+                "#de4968",
+                10,
+                "#8c2981",
+                15,
+                "#3b0f70",
+                20,
+                "#000004"
+              ],
+              'fill-opacity': 0.5
+        },
+        'layout': {
+            'visibility': 'none',
+        }
+    });
+
+        map.addLayer({
+        id:'access3',
+        type:'fill',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.3qq5mdb6'
+        },
+        'source-layer': 'Accessibility_3-c4l8bv',
+        'paint': {
+            'fill-color':
+            [
+               "step",
+                ["get", "k_complexity"],
+                "#fcfdbf",
+                3,
+                "#fe9f6d",
+                5,
+                "#de4968",
+                10,
+                "#8c2981",
+                15,
+                "#3b0f70",
+                20,
+                "#000004"
+              ],
+              'fill-opacity': 0.5
+        },
+        'layout': {
+            'visibility': 'none',
+        }
+    });
+
+    map.addLayer({
+        id:'floodzone',
+        type:'fill',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.brylfns0'
+        },
+              'source-layer': 'Flood_Risk_Areas-1jjt6w',
+
+            'paint': {
+            'fill-opacity': 0.5,
+            'fill-color': [
+               "step",
+                ["get", "gridcode"],
+                "#ADD8E6",
+                2,
+                "#ADD8E6",
+                3,
+                "#ADD8E6",
+                4,
+                "#ADD8E6"
+              ],
+        },
+
+        'layout': {
+            'visibility': 'none',
+        }
+    });
+
+        map.addLayer({
+        id:'slope',
+        type:'fill',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.5wi8ayv1'
+        },
+        'source-layer': 'Slope_2008_f-cfz0jz',
+
+            'paint': {
+            'fill-color': [
+               "step",
+                ["get", "gridcode"],
+                "#FFCE03",
+                10,
+                "#FD9A01",
+                20,
+                "#FD6104",
+                30,
+                "#FF2C05",
+              ],
+            'fill-opacity': 0.5
+        },
+
+        'layout': {
+            'visibility': 'none',
+        }
+    });
+
+        map.addLayer({
+        id:'informal',
+        type:'line',
+        source:{
+            'type':'vector',
+            'url':'mapbox://jm2369.1tiuk55k'
+        },
+        'source-layer': 'informal_merged-cfkjgp',
+
+            'paint': {
+            'line-color': '#0b1c47',
+            'line-width': 2,
+            'line-opacity': 0.7
+        },
+
+        'layout': {
+            'visibility': 'none',
+        }
+    });
+    }
---
+++ final project_3/mapwebsite_template-main/javascript/story.js
@@ -0,0 +1,105 @@
+// Story
+var pageNum = 1;
+var backButton = d3.select("#story-back");
+var forwardButton = d3.select("#story-forward");
+var pageNumbers = d3.select("#storymode-controls-numbers");
+var storyHeader = d3.select("#story-header");
+var storyContent = d3.select("#story-text");
+var storyImage =document.getElementById("story-image");
+
+var stories = [
+
+    // layers: ['wei_data', 'ce_data', 'bge_data', 'comm_area', 'landuse', 'gentrification'];
+
+    { title: "Informal Settlements in City of Kigali",
+      description: "Rwanda is envisioning to accelerate urbanization to 70%, yet its only urbanization modality is the massive rural-to-urban migration. The unabated influx of poor rural population to urban cores generates informal housings, in which 4-8 people live on a tiny plot without access to electricity and basic services, which deteriorates the urban quality without a proportional increase in investment and employment opportunities. Not only do the informal settlements cause negative externality, but also pose dangers to the impoverished population.",
+      image: 'assets/images/story/story1.png',
+      flyTo: {
+        zoom: 11.32,
+        pitch: 36.72,
+        center: [30.12733, -1.92971],
+        bearing: 0
+      },
+     },
+    { title: "Situational Factors: Connectivity",
+      description: "The situational factors analyze the location of settlement and its connectivity to social and built infrastructure. The informal settlers occupy the affordable and less desirable area informally and becasue the affordable lands can be found in either the edge of the urban boundary in a regularized urban growth models and clusters within the urban boundary, informal settlement lack access to transport and social infrastructure that connects them to the urban fabric. In City of Kigali, the informal settlements were found not only in the urban boundary but in the concetrated urban area as well. However, the connectivity gap for the informal settlement could also be observed in dense urban area.",
+      layer: "situational",
+      //image: 'assets/images/story/Uphill.png',
+      flyTo: {
+        zoom: 14.68,
+        pitch: 45,
+        center: [30.08469, -1.90036],
+        bearing: 0
+      },
+    },
+    { title: "Site Factors: Exposure to Risks",
+      description: "The National Land Use and Development Master Plan 2020-2050 recommends that lands with the slope of 30-50% can be used for settlements as long as there are no risks from earthquakes, floods, and landslides and that slopes above 55% be planted with trees (MOE, 2020). Mountainous Rwanda is comprised of 22.9% of 30-55% slopes and 12.6% above 55% slopes. Among the 61.6% of Kigali’s population who live in informal settlements, about 9% are located on the steep slope of above 30% while 4% are within flood-prone areas (Murill et. al, 2021).",
+      layer: "site",
+      flyTo: {
+        zoom: 12,
+        pitch:45,
+        center: [30.03198, -1.87371],
+        bearing: 0
+      },
+    },
+        { title: "Nyamugare Cell",
+      description: "The Nyamugare Cell is an uphill sloped settlement with streams flowing parallel downhill and cutting through the roads that follow the terrain’s contour lines. The distribution of land plots is irregular, following clustered patterns. During the rainy season, these streams carry large volumes of rainwater at high speed, causing severe erosion, damaging poorly constructed houses and increasing the risk of landslides. The impacts of these topographic and environmental factors negatively affect the urban poor. They provoke economic and social losses and increase the poor’s general vulnerability.",
+      layer: "site",
+       image: 'assets/images/story/Uphill.png',
+      flyTo: {
+        zoom: 14,
+        pitch:45,
+        center: [30.04203, -1.92134],
+        bearing: 0
+      },
+    },
+]
+
+// Function to update the active layer
+
+// Update Story.
+function updateStory(storyObj) {
+
+    // Story vars.
+    var title = storyObj['title'];
+    var description = storyObj['description'];
+    var cameraSettings = storyObj['flyTo'];
+    var imageSrc = storyObj['image'];
+    // var layer = storyObj['layer'];
+
+    // Update the Storymode content.
+    storyHeader.text(title);
+    storyContent.text(description);
+    storyImage.innerHTML += '<img src="'+imageSrc+'" style="width:100px;height:auto;" />'; // change the style to adjust the image size.
+    // storyImage.append("svg:image")
+    //           .attr("xlink:href", image);
+    map.flyTo(cameraSettings);
+    // call to update to active layer
+};
+
+// Callbacks
+// Story mode click through FORWARD.
+backButton.on("click", function () {
+
+    // Update the Navigation bottom panel.
+    pageNum = pageNum - 1;
+    pageNumbers.text(pageNum + " of " + stories.length);
+    backButton.style( "visibility", (pageNum == 1) ? "hidden" : "visible" );
+    forwardButton.style( "visibility", (pageNum == stories.length) ? "hidden" : "visible" );
+
+    // Update the story.
+    updateStory(stories[pageNum-1]);
+});
+
+  // Story mode click through BACKWARD.
+forwardButton.on("click", function () {
+
+    // Update the Navigation bottom panel.
+    pageNum = pageNum + 1;
+    pageNumbers.text(pageNum + " of " + stories.length);
+    backButton.style( "visibility", (pageNum == 1) ? "hidden" : "visible" );
+    forwardButton.style( "visibility", (pageNum == stories.length) ? "hidden" : "visible" );
+
+    // Update the story.
+    updateStory(stories[pageNum-1]);
+});
